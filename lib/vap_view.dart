a845dee55e749087acb5ab75c3d0d6ff44e420ed
LONG KHOA <tailieuhay0706@gmail.com>
2023-11-27 16:57:33 +0700

    refactore code

--- lib/vap_view.dart
+++ lib/vap_view.dart
@@ -7,35 +7,17 @@ import 'package:flutter_vap/vap_view_for_ios.dart';
 
 class VapView extends StatelessWidget {
 
-  final double forcedAspectRatio;
-
-  const VapView({Key? key, this.forcedAspectRatio=0}) : super(key: key);
+  const VapView({Key? key,}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
-    Widget child = const SizedBox();
-
+    Widget child = const SizedBox.shrink();
     if (Platform.isAndroid) {
-      child = Container(
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.red, width: 1),
-        ),
-        child: VapViewForAndroid()
-      );
+      child = VapViewForAndroid();
     } else if (Platform.isIOS) {
       child = VapViewForIos();
     }
 
-    // if(forcedAspectRatio>0) {
-    //   return AspectRatio(
-    //     aspectRatio: forcedAspectRatio,
-    //     child: FittedBox(
-    //       fit: BoxFit.fill, //force aspect ratio
-    //       child: child,
-    //     ),
-    //   );
-    // }
     return child;
   }
 }
